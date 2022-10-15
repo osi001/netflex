d55acef5ae03472623d142f9ceeaa9143ca8a569
osi001 <85637672+osi001@users.noreply.github.com>
2022-10-15 17:32:58 +0100

    updates, ProfileScreen

--- src/App.js
+++ src/App.js
@@ -5,22 +5,33 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
-  const user = null;
+  // const user = useSelector(selectUser);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         //signed in
-        console.log(user);
+        dispatch(
+          login({
+            uid: user.uid,
+            email: user.email,
+          })
+        );
       } else {
         //signed out
+        dispatch(logout());
       }
     });
 
     return unsubscribe;
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="app">
@@ -30,6 +41,7 @@ function App() {
         ) : (
           <Routes>
             <Route path="/" element={<HomeScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
           </Routes>
         )}
       </Router>
--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./Nav.css";
+import { useNavigate } from "react-router-dom";
 
 function Nav() {
   const [show, handleShow] = useState(false);
+  const navigate = useNavigate();
 
   const transistorNavBar = () => {
     if (window.scrollY > 100) {
@@ -21,11 +23,13 @@ function Nav() {
     <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__contents">
         <img
+          onClick={() => navigate("/")}
           className="nav__logo"
           src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
           alt=""
         />
         <img
+          onClick={() => navigate("/profile")}
           className="nav__avatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
           alt=""
